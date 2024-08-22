80fdb13a6012d6b3f161d364bd84d321217e2b5b
netstermonir <monirhossain6163@gmail.com>
2024-08-22 21:49:23 +0600

    actions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${spaceGrotesk.className} bg-black`}>
+      <body className={`${spaceGrotesk.className} bg-black overflow-x-hidden`}>
         <main>{children}</main>
       </body>
     </html>
